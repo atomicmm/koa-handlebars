5912bc4fc2f901ebe4c3ab5a0dd49dbfa24d8f99
Atomic <atomicmm@gmail.com>
2016-05-04 00:57:04 +0800

    coding...

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const extname = require('path').extname;
 const join = require('path').join;
 const resolve = require('path').resolve;
 const _stat = require('fs').stat;
+const Q = require('q');
 
 const renderer = require('./lib/renderer');
 
@@ -21,59 +22,12 @@ const isHtml = (ext) => ext === 'html'
  */
 const toFile = (fileName, ext) => `${fileName}.${ext}`
 
-/**
- * `fs.stat` promisfied.
- */
-const stat = (path) => {
-    return new Promise((resolve, reject) => {
-        _stat(path, (err, stats) => {
-            if (err) reject(err)
-            resolve(stats)
-        })
-    })
-}
-
-/**
- * Get the right path, respecting `index.[ext]`.
- * @param  {String} abs absolute path
- * @param  {String} rel relative path
- * @param  {String} ext File extension
- * @return {Object} tuple of { abs, rel }
- */
-function getPaths(abs, rel, ext) {
-    return stat(join(abs, rel)).then((stats) => {
-            if (stats.isDirectory()) {
-                // a directory
-                return {
-                    rel: join(rel, toFile('index', ext)),
-                    abs: join(abs, dirname(rel), rel)
-                }
-            }
-
-            // a file
-            return {
-                rel,
-                abs
-            }
-        })
-        .catch((e) => {
-            // not a valid file/directory
-            if (!extname(rel)) {
-                // Template file has been provided without extension
-                // so append to it to try another lookup
-                return getPaths(abs, `${rel}.${ext}`, ext)
-            }
-
-            throw e
-        })
-}
-
 module.exports = (path, opts) => {
     opts = defaults(opts || {}, {
         extension: 'html', // 默认后缀
-        viewPath: '', //视图目录
-        layoutPath: '', //布局视图目录
-        partialPath: '', //局部视图目录
+        viewPath: 'views', //视图目录
+        layoutPath: 'layouts', //布局视图目录
+        partialPath: 'partials', //局部视图目录
         defaultLayout: 'main.html' //默认布局视图名字
     })
 
@@ -84,6 +38,8 @@ module.exports = (path, opts) => {
             return next();
         }
 
+        renderer.init(opts); // init the renderer;
+
         /**
          * Render `view` with `locals` and `koa.ctx.state and handlebar opts`.
          *
@@ -91,25 +47,19 @@ module.exports = (path, opts) => {
          * @param {Object} locals
          * @param {Object} opts
          */
-        ctx.render = function(relPath, locals, opts) {
+        ctx.render = function(view, locals, opts) {
             if (locals == null) {
                 locals = {}
             }
 
-            let ext = (extname(relPath) || '.' + opts.extension).slice(1)
+            debug('render `%s` with %j', paths.rel, state)
+            const state = ctx.state ? Object.assign(locals, ctx.state) : locals
+            ctx.type = 'text/html';
 
-            return getPaths(path, relPath, ext).then((path) => {
-                const state = ctx.state ? Object.assign(locals, ctx.state) : locals
-                debug('render `%s` with %j', paths.rel, state)
-
-                ctx.type = 'text/html'
-
-                return renderer
-                    .render(resolve(paths.abs, paths.rel), state)
-                    .then((html) => {
-                        ctx.body = html;
-                    })
+            return renderer.render(view,state,opts).then(html =>{
+                ctx.body = html;
             });
+
         }
 
         return next();
